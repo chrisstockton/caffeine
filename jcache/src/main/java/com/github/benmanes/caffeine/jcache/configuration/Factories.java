cf86e4d2b577f79e5542048e9c2c41887149a140
Chris Stockton <christopher.stockton@target.com>
2016-08-24 12:00:07 -0700

    throw exception if bad loaderFactory param is passed

--- jcache/src/main/java/com/github/benmanes/caffeine/jcache/configuration/Factories.java
+++ jcache/src/main/java/com/github/benmanes/caffeine/jcache/configuration/Factories.java
@@ -4,19 +4,19 @@
 package com.github.benmanes.caffeine.jcache.configuration;
 
 import javax.cache.configuration.Factory;
-import javax.cache.configuration.FactoryBuilder;
 
 /**
  *
  */
 public class Factories<T> {
 
-    public static <T> Factory<T> getFactory(String className, String loaderFactory) {
-        switch (loaderFactory) {
-        case "guice":
-            return GuiceFactory.of(className);
-        default:
-            return FactoryBuilder.factoryOf(className);
-        }
+  public static <T> Factory<T> getFactory(String className, String loaderFactory) {
+    switch (loaderFactory) {
+      case "guice":
+        return GuiceFactory.of(className);
+      default:
+        throw new RuntimeException(
+            "Unable to create a Factory for: " + loaderFactory + " for className: " + className);
     }
+  }
 }
